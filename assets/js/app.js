5b900476a444ac7036a0d633c70cc42f0e41ae7f
unknown <julius.kamau@zilojo.com>
2021-06-09 17:39:06 +0300

    home

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,46 @@
 $(document).foundation();
+$(document).ready(function(){
+    $('.thumbnailcarousel .owl-carousel').owlCarousel(
+        {
+            loop:true,
+            margin:40,
+            nav:true,
+            dots:false,
+            autoplay: true,
+            responsive:{
+                0:{
+                    items:6
+                },
+                600:{
+                    items:8
+                },
+                1000:{
+                    items:8
+                }
+            }
+        }
+    );
+  });
+
+  $(document).ready(function(){
+    $('.owl-carousel.carousel-people').owlCarousel(
+        {
+            loop:true,
+            margin:40,
+            nav:true,
+            dots:false,
+            autoplay: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        }
+    );
+  });
